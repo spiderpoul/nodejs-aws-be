895af9007e075926f5f6940836b0202f8f5c8c8a
Pavel_Uvarov <pavel_uvarov@epam.com>
2020-11-16 10:59:27 +0300

    fix paths

--- import-service/handlers/importProductsFile.ts
+++ import-service/handlers/importProductsFile.ts
@@ -1,7 +1,11 @@
 import AWS from "aws-sdk";
-import { APIGatewayProxyHandler } from "aws-lambda";
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
 import "source-map-support/register";
-import { BUCKET_NAME as BUCKET, BUCKET_REGION, UPLOADED_FOLDER } from "../constants";
+import {
+    BUCKET_NAME as BUCKET,
+    BUCKET_REGION,
+    UPLOADED_FOLDER,
+} from "../constants";
 import { processError, processResponse } from "../utils";
 
 export const importProductsFile: APIGatewayProxyHandler = async (
@@ -10,21 +14,20 @@ export const importProductsFile: APIGatewayProxyHandler = async (
 ) => {
     const { name } = event.queryStringParameters;
 
-    try {
-        const s3 = new AWS.S3({
-            region: BUCKET_REGION,
-            signatureVersion: "v4",
+    const s3 = new AWS.S3({
+        region: BUCKET_REGION,        
+    });
+
+    const params = {
+        Bucket: BUCKET,
+        Key: `${UPLOADED_FOLDER}/${name}`
+    };
+
+    return new Promise<APIGatewayProxyResult>((resolve, reject) => {
+        s3.getSignedUrl("getObject", params, (error, url) => {
+            if (error) reject(processError(error));
+
+            resolve(processResponse(url));
         });
-
-        const params = {
-            Bucket: BUCKET,
-            Key: `${UPLOADED_FOLDER}/${name}`         
-        };
-
-        const signedUrl = await s3.getSignedUrl("getObject", params);
-
-        return processResponse(signedUrl);
-    } catch (err) {
-        return processError(err);
-    }
+    });
 };
